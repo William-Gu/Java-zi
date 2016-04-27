c4d7240cd9ffa761e90b2041f3832875faa4d2a9
William Gu <gujungujun@qq.com>
2016-04-27 12:48:46 +0800

    1
    1

---
+++ Example_4/src/com/liba/a1.java
@@ -0,0 +1,12 @@
+package com.liba;
+import javax.swing.*;
+public class a1 {
+	public static void main(String[] arge){
+		JFrame mw=new JFrame("I`m thr first window.");
+		mw.setSize(250,200);
+		JButton button=new JButton("I`m the first button.");
+		button.setSize(100,100);
+		mw.getContentPane().add(button);
+		mw.setVisible(true);
+	}
+}
